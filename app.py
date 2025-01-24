2cdda01df73f79824d381d4f049f93e93cc0061c
tejasdgangurde <tejasdgangurde@gmail.com>
2025-01-24 10:12:11 +0000

    tejas

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
- return "Hello, World!"
+ return "Hello from Tejas"
 if __name__ == "__main__":
  app.run(host="0.0.0.0", port=5000)
 
